79cf325cb182642ebcedb450c8074e3ffa3cabc6
Travis <Travis@TRAV-MB.local>
2023-03-28 21:29:47 -0700

    final fix

--- train.py
+++ train.py
@@ -9,8 +9,7 @@ model = AutoTS(forecast_length=12,
 
 if exists('data.csv'):
   df = pd.read_csv('data.csv')
-  df['ds'] = pd.to_datetime(df['ds'])
-  model = model.fit(df, date_col='ds', value_col='y', id_col=None)
+  model = model.fit(df, date_col='ds', value_col='y')
   model.export_template('model.csv', models='best',
                       n=15, max_per_model_class=3)
   prediction = model.predict()
